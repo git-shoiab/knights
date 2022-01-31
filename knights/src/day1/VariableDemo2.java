203c14bcafb07f03bd424dcac5f3659740e3bd10
hit300b1 <hit300b1@gmail.com>
2022-01-31 20:12:25 +0530

    aa

---
+++ knights/src/day1/VariableDemo2.java
@@ -0,0 +1,43 @@
+package day1;
+//Initialization
+public class VariableDemo2 {
+	public static void main(String[] args) {
+		//Number types
+		//1. byte 2. short 3. int 4. long
+		//byte - 8 bits
+		
+		byte b=-128;//minimum value
+		b=127;//max value
+		
+		//16 bit
+		short s=32_767;//VALUE IS assigned to variable s of type short
+		s=-327_68;
+		
+		//32 bit
+		int i=-2147483648;
+		i=2147483647;
+		
+		//64 bit
+		long l=-9223372036854775808L;//its compulsory that you have to give L at the end
+		l=-9223372036854775807L;
+		
+		//32 bit
+		float f=100;
+		f=100.24f;//its compulsory to give f for decimal values
+		
+		//64 bit
+		double d=343242;
+		d=324324.4555;
+		
+		char c=97;//this is number but it represents 'a' of ASCII
+		
+		c='a';
+		
+		boolean married=true;
+				married=false;
+				
+				
+		
+		
+	}
+}
