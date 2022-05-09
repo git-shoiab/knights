23d6f68ea5032cf91daccfbef11f53fdd6053473
hit300b1 <hit300b1@gmail.com>
2022-05-09 20:06:37 +0530

    9-may

--- knights/src/utilpack/ArrayListDemo.java
+++ knights/src/utilpack/ArrayListDemo.java
@@ -1,14 +1,25 @@
 package utilpack;
 //https://fluvid.com/videos/detail/Ee1-6HDG6OIQYrdOw#.YnUsF13iE0M.link
+
+//https://fluvid.com/videos/detail/QKZX3hqdvOC9qGeYp#.Ynkm3Ax0Zx8.link
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Vector;
 
 public class ArrayListDemo {
 	public static void main(String[] args) {
 		int i[]= {10,20,30};
 		
-		ArrayList list=new ArrayList();
+		//ArrayList list=new ArrayList();
+		//LinkedList<Integer> list=new LinkedList<>();
+		Vector<Integer> list=new Vector<>();
+		list.add(60);
+		list.add(55);
 		list.add(10);
 		list.add(20);
 		list.add(30);
@@ -31,6 +42,7 @@ public class ArrayListDemo {
 		}
 		
 		Iterator iter=list.iterator();
+		//list.add(50);
 		while(iter.hasNext()) {
 			System.out.println(iter.next());
 		}
@@ -42,5 +54,28 @@ public class ArrayListDemo {
 		while(liter.hasPrevious()) {
 			System.out.println(liter.previous());
 		}
+		System.out.println("............................");
+		Enumeration<Integer> en=list.elements();
+		list.add(50);
+		while(en.hasMoreElements()) {
+			System.out.println(en.nextElement());
+		}
+		
+		Collections.sort(list);
+		System.out.println(list);
+		Collections.synchronizedCollection(list);//for vector not needed
+		
+		//Collections.sort(list,(n1,n2)->{return n2.compareTo(n1);}); //-this is best
+		Collections.sort(list,new MyComparator());
+		System.out.println(list);
+		
+	}
+}
+
+class MyComparator implements Comparator<Integer>{
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o2.compareTo(o1);
 	}
 }
